ed7349774d5675ffb3b52e2813516bced35f4275
Yorick Dupon <2090180+yorickdupon@users.noreply.github.com>
2024-11-27 20:49:52 +0100

    footer?

--- templates/footer.js
+++ templates/footer.js
@@ -3,7 +3,9 @@ import settings from "../settings.json";
 
 export const renderFooter = html.node`<footer class="footer" role="contentinfo">
   <div class="tagline">
-  
-  </a>
-</footer>
+   <footer>
+        <small>
+        foto: Pieter-Jan Vanstockstraeten / Photo News
+      </small>
+    </footer>
 `;
